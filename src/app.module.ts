3d5b3f101a666faad26ca01815bb831d8cb8d2e1
Oleg Kamenshikov <burnsred94@gmail.com>
2022-08-02 15:39:56 +0300

    feat: implements controllers and connect db mongo it docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
+    AuthModule,
+    TopPageModule,
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
